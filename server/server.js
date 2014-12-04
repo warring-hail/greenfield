b43528443664f38f17a360c8d018adf1a62620f1
mal <lom.michael@gmail.com>
2014-12-04 12:31:03 -0800

    setup server file and additional node dependencies

--- server/server.js
+++ server/server.js
@@ -1,5 +1,27 @@
 var express = require('express');
+var mongoose = require('mongoose');
 
 var app = express();
+
+//connects mongoose to the 'test' database
+mongoose.connect('mongodb://localhost/test');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+	var userSchema = mongoose.Schema({
+		name: String
+	})
+	var User = mongoose.model('User', userSchema);
+	var mike = new User({ name: 'Mike' })
+	mike.save(function(err, mike){
+		if (err) return console.error(err);
+		console.log(mike.name + 'added to db')
+	})
+});
+
+// require('../config/middleware.js')(app, express);
+
+app.listen(8000);
 
 module.exports = app;
