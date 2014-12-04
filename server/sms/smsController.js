783fb2f91b274b1fb6908c1fe0989e8a29bdf618
Hack Reactor Students <accounts@hackreactor.com>
2014-12-04 15:37:36 -0800

    Created smsTester html and modified sms controller

--- server/sms/smsController.js
+++ server/sms/smsController.js
@@ -1,13 +1,7 @@
-// Make a post request to the correct URL
-///2010-04-01/Accounts/{AccountSid}/Messages/{MessageSid}
-
-//require the Twilio module and create a REST client
 TWILIO_AUTH_TOKEN = "33221289c2cd812e654e1a58f51467a5";
 TWILIO_SID = "AC9399d6f79b86ce3becf0e7b98f2a45a4";
 
 var client = require('twilio')(TWILIO_SID, TWILIO_AUTH_TOKEN);
-
-//Send an SMS text message
 
 var generateCode = function(userPhone) {
     var code = Math.floor(Math.random()*90000) + 10000;
@@ -17,7 +11,7 @@ var generateCode = function(userPhone) {
 
 module.exports = {
     sendVerification: function() {
-        var userPhone = req.body.userPhone;
+        var userPhone = req.body.phone;
         var code = generateCode(userPhone);
         client.sendMessage({
             to:'+1'+userPhone, // Any number Twilio can deliver to
@@ -31,8 +25,11 @@ module.exports = {
         });
     },
     verifyCode: function() {
-        var userPhone = req.body.userPhone;
-        var submittedCode = req.body.code;
+        var userPhone = req.body.phone;
+        var userCode = req.body.code;
+
         // Lookup code in database by userPhone
+        // If it's the correct code, send a response 201
+        // If it's not, return false, if it's not send a 404
     }
 
