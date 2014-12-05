30fa229e1248976abbde6a6fd22bc42af1ad4cf2
Hack Reactor Students <accounts@hackreactor.com>
2014-12-04 19:40:39 -0800

    Finished post request handlers for verify and send sms

--- server/sms/smsController.js
+++ server/sms/smsController.js
@@ -2,45 +2,47 @@ require('dotenv').load();
 var AuthCodeModel = require('./authCodeModel');
 var client = require('twilio')(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN);
 
+// Generate a random authentication code and save it in the db
 var generateCode = function(userPhone) {
     var code = Math.floor(Math.random()*90000) + 10000;
     var codeModel = new AuthCodeModel.AuthCode({ phone: userPhone, code: code });
     codeModel.save(function(err,codeModel) {
         if(err) throw err;
-        console.log("code saved");
     });
     return code;
 };
 
 module.exports = {
+    // Send an auth code to the user
     sendVerification: function(req, res) {
         var userPhone = req.body.phone;
         var code = generateCode(userPhone);
 
         client.sendMessage({
-            to:'+1'+userPhone, // Any number Twilio can deliver to
-            from: '+16508259600', // A number you bought from Twilio and can use for outbound communication
-            body: "Enter "+code+" on the signup page to verify your account." // body of the SMS message
-        }, function(err, responseData) { //this function is executed when a response is received from Twilio
-            if(err) throw err;
-            if(!err) {
-                console.log("Message sent",responseData.body);
+            to:'+1'+userPhone,
+            from: '+16508259600',
+            body: "Enter "+code+" on the signup page to verify your account."
+        }, function(err, responseData) {
+            if(err) {
+                throw err;
+                res.status(500).send({sent:false});
+            } else {
+                res.status(204).send({sent:true});
             }
         });
 
     },
+    // Check to see if the verification code exists in the DB
     verifyCode: function(req, res) {
         var userPhone = req.body.phone;
         var code = req.body.code;
-        console.log("USERPHONE",userPhone);
-        console.log("CODE",code);
         AuthCodeModel.AuthCode.find({ phone: userPhone, code: code }, function(err,data) {
             if(err) return console.error(err);
             if(data.length === 0) {
-
+                res.status(201).send({found:false});
+            } else {
+                res.status(201).send({found:true});
             }
-            console.log("Record",data);
-
         });
     }
 }
