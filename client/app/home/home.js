d8bea6c94f72ab83d83bc8be44e6f8659d19d5e3
Sebastian Tonkin <sgtonkin@gmail.com>
2014-12-08 12:24:46 -0800

    Finished styling homepage

--- client/app/home/home.js
+++ client/app/home/home.js
@@ -1,7 +1,6 @@
 angular.module('pledgr.home', [])
 
 .controller('HomeController', function() {
-  
   $('.uiContainer').addClass('home');
   $('.container-fluid').height('100%');
   $('.uiContainer').width('105%');
