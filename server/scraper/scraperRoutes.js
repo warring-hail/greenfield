ea90f4aa65d9dc74924899fa76c575b72b773932
Douglas Eichelberger <dduugg@gmail.com>
2014-12-08 18:13:10 -0800

    enable starting orgid for scraper

--- server/scraper/scraperRoutes.js
+++ server/scraper/scraperRoutes.js
@@ -2,4 +2,5 @@ var scraperController = require('./scraperController');
 
 module.exports = function(app) {
   app.get('/:count', scraperController.scrape);
+  app.get('/:count/:start', scraperController.scrape);
 };
